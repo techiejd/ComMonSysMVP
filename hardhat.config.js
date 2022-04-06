67d45f8b436ff0d5ee76df4f18edbc14d8d1197e
techiejd <62455039+techiejd@users.noreply.github.com>
2022-04-06 16:29:10 -0500

    Adds a test network and test scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,14 +9,17 @@ module.exports = {
   networks: {
     hardhat: {
       accounts: {
-        accountsBalance: "5000000000000000000000000"
-      }
+        accountsBalance: "5000000000000000000000000",
+      },
     },
     commonsys: {
       url: "https://137.184.238.79/rpc",
       chainId: 14,
-      accounts: [process.env.PRIVATEKEY]
-    }
+      accounts: [process.env.PRIVATEKEY],
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
   },
   solidity: "0.8.1",
 };
